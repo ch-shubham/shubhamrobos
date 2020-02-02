0629e8de34e0ae891f8c32a8d66b84232a7203a0
ch-shubham <shubham128166@gmail.com>
2020-02-02 12:09:40 +0530

    Updates

---
+++ precache-manifest.a03b755ad3b6940dcb0b9afe15b01f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e88de8acd2424b247b547c865358211",
+    "url": "/shubhamrobos/index.html"
+  },
+  {
+    "revision": "e93ccaa29925dbe062e0",
+    "url": "/shubhamrobos/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/shubhamrobos/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "e93ccaa29925dbe062e0",
+    "url": "/shubhamrobos/static/js/main.1dbc0f8b.chunk.js"
+  },
+  {
+    "revision": "d6ad77b1289f41824c22",
+    "url": "/shubhamrobos/static/js/runtime~main.b6e9eb85.js"
+  }
+]);
